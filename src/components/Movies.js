7d23c96c13b1fd4829585f03b2be52d90b4dfd1a
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-11-01 21:38:18 +0300

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,3 @@
-// import React from "react";
-// import { movies } from "../data";
-
-// function Movies() {
-//   return <div>{/*{code here}*/}</div>;
-// }
-
-// export default Movies;
 import React from "react";
 import { movies } from "../data";
 
