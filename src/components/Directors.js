ca6197478c667b2403274af7cbe1839d09145c95
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-11-01 21:35:23 +0300

    wednesday

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,30 @@
+// import React from "react";
+// import { directors } from "../data";
+
+// function Directors() {
+//   return <div>{/*{code here}*/}</div>;
+// }
+
+// export default Directors;
 import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/movies" activeClassName="active">
+        Movies
+      </NavLink>
+      <NavLink to="/directors" activeClassName="active">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" activeClassName="active">
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
